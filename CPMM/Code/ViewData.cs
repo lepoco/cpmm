cea1798ab25095783be74bae8c114dcd3492e51d
Pomianowski <kontakt@rapiddev.pl>
2022-02-22 22:01:51 +0100

    Open dialog selector for mod archive

--- CPMM/Code/ViewData.cs
+++ CPMM/Code/ViewData.cs
@@ -1,4 +1,9 @@
-﻿using System.ComponentModel;
+﻿// This Source Code Form is subject to the terms of the GNU GPL-3.0.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
+// Copyright (C) 2022 Leszek Pomianowski and CPMM Contributors.
+// All Rights Reserved.
+
+using System.ComponentModel;
 
 namespace CPMM.Code
 {
@@ -9,6 +14,9 @@ namespace CPMM.Code
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        /// <summary>
+        /// Updates property by reference if value changed.
+        /// </summary>
         protected void UpdateProperty<T>(ref T property, object value, string propertyName)
         {
             if (property == null || property.Equals(value))
--- CPMM/Views/Pages/Install.xaml.cs
+++ CPMM/Views/Pages/Install.xaml.cs
@@ -5,8 +5,9 @@
 
 using CPMM.Code;
 using Lepo.i18n;
+using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
+using WPFUI.Controls.Interfaces;
 
 namespace CPMM.Views.Pages
 {
@@ -19,25 +20,63 @@ namespace CPMM.Views.Pages
             get => _modificationPath;
             set => UpdateProperty(ref _modificationPath, value, nameof(ModificationPath));
         }
+
+        private bool _dialogMultiSelect = false;
+
+        public bool DialogMultiSelect
+        {
+            get => _dialogMultiSelect;
+            set => UpdateProperty(ref _dialogMultiSelect, value, nameof(DialogMultiSelect));
+        }
     }
 
     /// <summary>
     /// Interaction logic for Install.xaml
     /// </summary>
-    public partial class Install : Page
+    public partial class Install : INavigable
     {
         internal InstallData InstallDataStack { get; } = new();
+
+        internal OpenFileDialog DialogSelector;
 
         public Install()
         {
             InitializeComponent();
+            DialogSelector = InitializeDialog();
 
             DataContext = InstallDataStack;
         }
 
+        public void OnNavigationRequest(INavigation sender, object current)
+        {
+            // Navigated
+        }
+
+        private OpenFileDialog InitializeDialog()
+        {
+            var dialogFilter = Translator.String("global.dialog.cyberMods") + " (*.7z;*.zip;*.rar)|*.7z;*.zip;*.rar";
+            dialogFilter += "|All we had to do, was follow the damn train, CJ!(*.hotcoffee)|*.exe";
+            dialogFilter += "|" + Translator.String("global.dialog.allFiles") + " (*.*)|*.*";
+
+            return new OpenFileDialog()
+            {
+                Title = Translator.String("global.dialog.selectMods"),
+                Filter = dialogFilter,
+                CheckPathExists = true,
+                Multiselect = InstallDataStack.DialogMultiSelect
+            };
+        }
+
         private void ButtonSelect_OnClick(object sender, RoutedEventArgs e)
         {
-            // Select
+            if (!DialogSelector.ShowDialog() ?? false)
+                return;
+
+#if DEBUG
+            System.Diagnostics.Debug.WriteLine($"INFO | {DialogSelector.FileName} selected, Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId}", "CPMM");
+#endif
+
+            // File_modSelector.FileNames
         }
     }
 }
