064ca6b416f9f62c418475148989d5f512547d19
Pomianowski <kontakt@rapiddev.pl>
2022-02-23 12:34:44 +0100

    Mod class structure

--- Modules/CPMM.Core/Mods/IMod.cs
+++ Modules/CPMM.Core/Mods/IMod.cs
@@ -5,10 +5,43 @@
 
 namespace CPMM.Core.Mods
 {
+    /// <summary>
+    /// Represents instance of the modification.
+    /// </summary>
     public interface IMod
     {
+        public int Id { get; }
+
+        public int Priority { get; }
+
+        public bool Enabled { get; }
+
+        public bool Installed { get; }
+
+        public string Name { get; }
+
+        public string Author { get; }
+
+        public string Website { get; }
+
+        public string ArchiveName { get; }
+
+        public string SourcePath { get; }
+
+        public string SourceBackupPath { get; }
+
+        public Version Version { get; }
+
         public Core.Mods.Location Location { get; }
 
         public Core.Mods.Category Category { get; }
+
+        public DateTime DateInstalled { get; }
+
+        public DateTime DateModified { get; }
+
+        public IEnumerable<string> Files { get; }
+
+        public IEnumerable<string> FilesOverridden { get; }
     }
 }
--- Modules/CPMM.Core/Mods/Mod.cs
+++ Modules/CPMM.Core/Mods/Mod.cs
@@ -5,10 +5,44 @@
 
 namespace CPMM.Core.Mods
 {
+    /// <summary>
+    /// Represents instance of the modification.
+    /// </summary>
+    [Serializable]
     public class Mod : IMod
     {
+        public int Id { get; internal set; } = 0;
+
+        public int Priority { get; internal set; } = 0;
+
+        public bool Enabled { get; internal set; } = false;
+
+        public bool Installed { get; internal set; } = false;
+
+        public string Name { get; internal set; } = String.Empty;
+
+        public string Author { get; internal set; } = String.Empty;
+
+        public string Website { get; internal set; } = String.Empty;
+
+        public string ArchiveName { get; internal set; } = String.Empty;
+
+        public string SourcePath { get; internal set; } = String.Empty;
+
+        public string SourceBackupPath { get; internal set; } = String.Empty;
+
+        public Version Version { get; internal set; } = new(1, 0, 0);
+
         public Core.Mods.Location Location { get; internal set; } = Core.Mods.Location.Unknown;
 
         public Core.Mods.Category Category { get; internal set; } = Core.Mods.Category.Unknown;
+
+        public DateTime DateInstalled { get; internal set; } = DateTime.Now;
+
+        public DateTime DateModified { get; internal set; } = DateTime.Now;
+
+        public IEnumerable<string> Files { get; internal set; } = new string[] { };
+
+        public IEnumerable<string> FilesOverridden { get; internal set; } = new string[] { };
     }
 }
